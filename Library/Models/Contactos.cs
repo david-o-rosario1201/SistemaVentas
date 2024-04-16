e4c5bebd261d3ca16cdb702aeed8a1d74cab18d0
david-o-rosario1201 <finnnn023@gmail.com>
2024-04-16 17:18:15 -0400

    Creación del modelo Contactos.

---
+++ Library/Models/Contactos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Models;
+
+public class Contactos
+{
+	[Key]
+	public int ContactoId { get; set; }
+
+    public string Contacto { get; set; }
+}
--- SistemaVentasDavid/SistemaVentasDavid/Data/ApplicationDbContext.cs
+++ SistemaVentasDavid/SistemaVentasDavid/Data/ApplicationDbContext.cs
@@ -7,5 +7,6 @@ namespace SistemaVentasDavid.Data
 	public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 	{
 		public DbSet<TiposContribuyente> TiposContribuyente { get;  set; }
+		public DbSet<Contactos> Contactos { get;  set; }
 	}
 }
